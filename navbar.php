4358693d15ce10513cdadfe1464a6d8bf475b5b2
Garima Singh <63351001+garima190@users.noreply.github.com>
2021-11-17 23:03:24 +0530

    Create navbar.php

---
+++ navbar.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+body {margin:0;}
+.nav{
+    margin-bottom:13px;
+}
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  background-color: #334;
+  position: fixed;
+  top: 0;
+  width: 100%;
+}
+
+li {
+  float: left;
+}
+
+li a {
+  display: block;
+  color: white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+}
+
+li a:hover:not(.title) {
+  background-color: #c7371f;
+}
+.title {
+  background-color: #c7371f;
+}
+
+</style>
+</head>
+<body>
+   <nav class="Nav">
+    <ul>
+    <li>
+    <a class="title" href="#">Sgrip Bank</a>
+    </li>
+    <li>
+    <a href="index.php">Home</a>
+    </li>
+    <li>
+    <a href="AllCustomerInfo.php">View Customer Details</a>
+    </li>
+    <li>
+    <a href="FundTransfer.php">Fund Transfer</a>
+    </li>
+    <li>
+    <a href="History.php">Transaction History</a>
+    </li>
+    </ul>
+   </nav>
+<body>
+</html>
+
