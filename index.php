ad0f9ff1b070d77abe457fa47527ca00e4ca6b17
Garima Singh <63351001+garima190@users.noreply.github.com>
2021-11-17 23:01:24 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+
+<head>
+  <meta charset="utf-8">
+  <title>Sgrip Bank</title>
+  <style>
+    body {
+      padding-top: 100px;
+      font-size:30px;
+      background: #e9e3e2;
+      
+    }
+    .center{
+      padding-top:5px;
+      background: #ddc2bc;
+      display: block;
+      margin-top: 20px;
+      margin-left: auto;
+      margin-right: auto;
+      width: 52%;
+    }
+    footer {
+            background-color:#c1506a;
+            position: absolute;
+            left: 0;
+            bottom: 0;
+            height: 65px;
+            width: 100%;
+           
+        }
+ 
+  </style>
+</head>
+<body>
+  <?php include('navbar.php'); ?>
+  
+  <div class="center">
+      <h1 style="text-align: center">Sgrip Bank<h1>
+      <img class ="center" src="bankImage.jpg" alt="" width="150" height="300">
+      
+  </div>
+  <footer style="text-align: center">
+            <p style="font-size:15px">Designed and Coded by- Garima Singh</p>
+            <p style="font-size:15px">E-mail- garimaXXXX@gmail.com</p>
+  </footer>
+</body>
+</html>
